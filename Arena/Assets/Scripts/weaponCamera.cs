3ba888a2a6daede284eed363bc308eccc5b712b6
Axel Lyra <axellyra@gmail.com>
2025-05-29 20:45:10 +0200

    Horizontal sword swing added

--- Arena/Assets/Scripts/weaponCamera.cs
+++ Arena/Assets/Scripts/weaponCamera.cs
@@ -1,18 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class weaponCamera : MonoBehaviour
 {
-    public GameObject mainCamera;
+
+    InputAction attackAction;
+    public Animator weaponAnimator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        attackAction = InputSystem.actions.FindAction("Attack");
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = mainCamera.transform.position;
-        gameObject.transform.rotation = mainCamera.transform.rotation;
+        gameObject.transform.position = GameObject.FindWithTag("MainCamera").transform.position;
+        gameObject.transform.rotation = GameObject.FindWithTag("MainCamera").transform.rotation;
+        if (attackAction.WasPressedThisFrame())
+        {
+            weaponAnimator.SetTrigger("Attack");
+        }
     }
 }
