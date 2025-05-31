39adc1e135a2fe1092710692c09881e5196e7835
Axel Lyra <axellyra@gmail.com>
2025-05-31 18:03:36 +0200

    Migrated weapon script and added animation events

--- Arena/Assets/Scripts/PlayerAttack.cs
+++ Arena/Assets/Scripts/PlayerAttack.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class weaponCamera : MonoBehaviour
+public class PlayerAttack : MonoBehaviour
 {
     public AudioClip attackSound;
-    public Animator weaponAnimator;
+    Animator weaponAnimator;
     InputAction attackAction;
     AudioSource audioSource;
 
@@ -12,6 +12,7 @@ public class weaponCamera : MonoBehaviour
     void Start()
     {
         attackAction = InputSystem.actions.FindAction("Attack");
+        weaponAnimator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = attackSound;
     }
@@ -19,13 +20,17 @@ public class weaponCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = GameObject.FindWithTag("MainCamera").transform.position;
-        gameObject.transform.rotation = GameObject.FindWithTag("MainCamera").transform.rotation;
         if (attackAction.WasPressedThisFrame() && weaponAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
         {
             weaponAnimator.SetTrigger("Attack" + Random.Range(1, 7));
             audioSource.pitch = Random.Range(0.9f, 1.1f);
             audioSource.Play();
         }
+    }
+
+    // Call when the weapon checks if it hits an enemy
+    public void WeaponHit()
+    {
+        print("Now the code checks what I hit!!");
     }
 }
