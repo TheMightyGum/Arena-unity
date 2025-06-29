4553869f69c000f230f30198086802cbc330ceba
Axel Lyra <axellyra@gmail.com>
2025-06-29 02:42:59 +0200

    Added fadein / out animation

--- Arena/Assets/Scripts/UIManager.cs
+++ Arena/Assets/Scripts/UIManager.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class UIManager : MonoBehaviour
 {
     InputAction openMenu;
     [SerializeField] GameObject statsMenu;
+    [SerializeField] Image fadeout;
+    private Color fadeColor;
+    public float fadeTime;
+    public float darkTime;
     public bool isPaused;
+    public bool isFading;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         openMenu = InputSystem.actions.FindAction("StatsMenu");
 
+        fadeout.gameObject.SetActive(true);
+        setFadeAlpha(0f);
+
         isPaused = false;
+        isFading = false;
         Time.timeScale = 1f;
 
         Cursor.lockState = CursorLockMode.Locked;
@@ -22,25 +33,60 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (openMenu.WasPressedThisFrame())
+        if (openMenu.WasPressedThisFrame() && !isFading)
         {
-            if (statsMenu.activeSelf)
+            if (statsMenu.activeSelf) //If already in menu
             {
-                statsMenu.SetActive(false);
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-
-                isPaused = false;
-                Time.timeScale = 1f;
-            } else
+                StartCoroutine(FadeInto(statsMenu, false));
+            }
+            else
             {
-                statsMenu.SetActive(true);
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-
-                isPaused = true;
-                Time.timeScale = 0f;
+                StartCoroutine(FadeInto(statsMenu, true));
             }
         }
     }
+
+    public IEnumerator FadeInto(GameObject menu, bool setActive)
+    {
+        if (setActive)
+        {
+            isPaused = true;
+            Time.timeScale = 0f;
+        } else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        isFading = true;
+        float _time = 0f;
+        while(_time < fadeTime)
+        {
+            _time += Time.unscaledDeltaTime;
+            setFadeAlpha(_time / fadeTime);
+            yield return null;
+        }
+        menu.SetActive(setActive);
+        _time += darkTime;
+        while (_time > 0)
+        {
+            _time -= Time.unscaledDeltaTime;
+            setFadeAlpha(_time / fadeTime);
+            yield return null;
+        }
+        fadeColor.a = 1f;
+        isFading = false;
+
+        if (setActive)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        } else
+        {
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+    }
+
+    void setFadeAlpha(float alpha) { fadeout.color = new Color(fadeout.color.r, fadeout.color.g, fadeout.color.b, alpha); }
 }
