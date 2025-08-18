c064ff8698d9d8e8037b04f495b6bfd3cda642d3
Axel Lyra <axellyra@gmail.com>
2025-08-18 23:40:42 +0200

    renamed script

--- Arena/Assets/Scripts/Scriptable objects/Items/itemMaterials.cs
+++ Arena/Assets/Scripts/Scriptable objects/Items/itemMaterials.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "materials", menuName = "Scriptable Objects/materials")]
-public class materials : ScriptableObject
+[CreateAssetMenu(fileName = "itemMaterials", menuName = "Scriptable Objects/itemMaterials")]
+public class itemMaterials : ScriptableObject
 {
-    
+    public string materialName;
+
+    public int bonusToHit;
+    public int bonusMinDamage;
+    public int bonusMaxDamage;
+
+    public int bonusAC;
+    public int deteriorationRate; //Percentage from damage dealt removed from condition
+
+    public int metalTier; //0:mundane weapon, 1:Harms ice golems, 2:Harms Medusa & Stone Golem, 3:Harms everything
+    public bool effectiveAgainstVampires; //Deals double damage
 }
